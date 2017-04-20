116f822a2db340acd02ec78fcf17537468a84437
Kirk Hayes (l0gan) <kirk.paul.hayes+github@gmail.com>
2017-04-20 13:18:35 -0400

    formatting and add app selection by number

--- backHack.py
+++ backHack.py
@@ -1,13 +1,16 @@
-#! /bin/python
+#! /usr/bin/python
 import os
 import tarfile
 from distutils.version import LooseVersion
 import subprocess
+from subprocess import check_output
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description='Hacking some Android apps! Use backHack to perform Android Application File System Analysis on non-rooted devices. Running without arguments will run in interactive mode.')
 parser.add_argument('--app', '-a', help='Application name (e.g. com.niantic.pokemongo)')
 parser.add_argument('--listapps', '-l', action="store_true", help='List apps on device')
+parser.add_argument('--backup', '-b', action="store_true", help='Backup app')
+parser.add_argument('--restore', '-r', action="store_true", help='Restore app')
 
 
 
@@ -45,7 +48,7 @@ def mainmenu():
         for entry in options:
             print(entry, mainMenu[entry])
 
-        selection=raw_input("Please select an option:")
+        selection=raw_input("[*]  Please select an option:  ")
         if selection == "1":
             cls()
             appSelectMenu = {}
@@ -75,36 +78,50 @@ def mainmenu():
                 for entry in options:
                     print(entry, appSelectMenu[entry])
 
-                selection=raw_input("Please select an option:")
+                selection=raw_input("[*]  Please select an option:  ")
                 if selection == "1":
                     cls()
-                    listApps()
-                    print("Review the preceding list then use option 2 to type in the name of the package")
+                    packs = listApps()
+                    appnumber = raw_input("[*] Enter number corresponding with the app you want to use: ")
+                    appName = packs[int(appnumber)]
+                    cls()
+                    print("[!] Your chosen app:  " + appName)
+                    break
                 elif selection == "2":
                     cls()
-                    appSearch=raw_input("Type in part of the name to search for: ")
+                    appSearch=raw_input("[*]  Type in part of the name to search for: ")
                     print("")
-                    os.system('adb shell pm list packages | find /I "' + appSearch + '"' if os.name == 'nt' else "adb shell pm list packages |  grep -i " + appSearch + " | cut -d: -f2")
+                    packs = check_output('adb shell pm list packages | find /I "' + appSearch + '"' if os.name == 'nt' else "adb shell pm list packages |  grep -i " + appSearch + " | cut -d: -f2", shell=True)
+                    packs = packs.split(":")
+                    packs = [i.split('\r\n', 1)[0] for i in packs]
+                    i = 0
+                    for pack in packs:
+                        print str(i) + ": " + pack
+                        i = i+1
                     print("")
-                    print("Copy the name of the app you want and use selection 3 to specify the app you are hacking")
+                    appnumber = raw_input("[*] Enter number corresponding with the app you want to use: ")
+                    appName = packs[int(appnumber)]
+                    cls()
+                    print("[!] Your chosen app:  " + appName)
+                    break
                 elif selection == "3":
                     cls()
-                    appName=raw_input("Please type in the package name:")
+                    appName=raw_input("[*]  Please type in the package name:")
                     cls()
-                    print("Your chosen app:  " + appName)
+                    print("[!] Your chosen app:  " + appName)
                     break
                 elif selection == "99":
                     cls()
                     break
                 else:
                     cls()
-                    print("Invalid Selection")
+                    print("[-] Invalid Selection")
         elif selection == "2":
             cls()
             if appName:
                 backupApp(appName)
             else:
-                print("You have not selected an app.  Please use option 1 to set your app.")
+                print("[-] You have not selected an app.  Please use option 1 to set your app.")
 
         elif selection == "3":
             cls()
@@ -112,14 +129,14 @@ def mainmenu():
             if appName:
                 restoreApp(andVer, appName)
             else:
-                print("You have not selected an app.  Please use option 1 to set your app.")
+                print("[-] You have not selected an app.  Please use option 1 to set your app.")
         elif selection =="99":
             cls()
             cleanup(appName)
             break
         else:
             cls()
-            print("Invalid Selection")
+            print("[-] Invalid Selection")
 
 def andVerCheck():
     andVerNum = subprocess.check_output("adb.exe shell getprop ro.build.version.release" if os.name == 'nt' else "adb shell getprop ro.build.version.release", shell=True)
@@ -131,7 +148,7 @@ def andVerCheck():
     return andVer
 
 def restoreApp(andVer, appName):
-    print("Repacking " + appName)
+    print("[*] Repacking " + appName)
     tar = tarfile.open(appName + "-rest.tar", "w", format=tarfile.USTAR_FORMAT)
     retar = open("fileList.txt", 'r')
     for name in retar.readlines():
@@ -140,49 +157,63 @@ def restoreApp(andVer, appName):
     tar.close()
     os.system("java -jar abe.jar "+ andVer + " " + appName + "-rest.tar " + appName + "-rest.ab")
     cls()
-    print("Repacking complete.")
-    print("Restoring " + appName)
+    print("[*] Repacking complete.")
+    print("[*] Restoring " + appName)
     os.system("adb.exe restore " + appName + "-rest.ab" if os.name == 'nt' else "adb restore " + appName + "-rest.ab")
 
 def cleanup(appName):
-    print("Cleaning Up")
+    print("[*] Cleaning Up")
     os.system("del fileList.txt "+ appName + ".* " + appName + "-* " if os.name == 'nt' else "rm fileList.txt "+ appName + ".* " + appName + "-* " if appName != '' else "echo Nothing to remove")
     os.system("rd /S /Q apps" if os.name == 'nt' else "rm -rf apps")
 
 def listApps():
-    os.system("adb.exe shell pm list packages" if os.name == 'nt' else "adb shell pm list packages")
+    packs = check_output("adb.exe shell pm list packages" if os.name == 'nt' else "adb shell pm list packages", shell=True)
+    #packs = os.popen("adb.exe shell pm list packages" if os.name == 'nt' else "adb shell pm list packages").read()
+    packs = packs.split(":")
+    packs = [i.split('\r\n', 1)[0] for i in packs]
+    i = 0
+    for pack in packs:
+        print str(i) + ": " + pack
+        i = i+1
+    return packs
+
 
 def backupApp(appName):
-    print("Backing up " + appName)
+    print("[*] Backing up " + appName)
     os.system("adb.exe backup -f " + appName + ".ab " + appName if os.name == 'nt' else "adb backup -f " + appName + ".ab " + appName)
-    print("Extracting " + appName + ".")
+    print("[*] Extracting " + appName + ".")
     os.system("java -jar abe.jar unpack " + appName + ".ab " + appName + ".tar")
     tar = tarfile.open(appName + ".tar")
     tar.extractall()
     tarList = open("fileList.txt", 'w')
     for member in tar.getmembers():
         tarList.write(str(member.name) + '\n')
-    tarList.close()
+        tarList.close()
     tar.close()
-    print("Extraction Complete.  Please review files under Apps.")
+    print("[*] Extraction Complete.  Please review files under apps folder.")
 
 def main():
     cls()
     mainmenu()
 
-def autoRun(app):
-    backupApp(app)
-    raw_input("Press Enter to restore App...")
-    andVer = andVerCheck()
-    restoreApp(andVer, app)
-    raw_input("Press Enter when finished to remove files...")
-    cleanup(app)
-
 if __name__ == '__main__':
     args = parser.parse_args()
     parser.set_defaults(listapps=False)
+    parser.set_defaults(backup=False)
+    parser.set_defaults(restore=False)
     if args.app:
-        autoRun(args.app)
+        if args.backup:
+            backupApp(args.app)
+        elif args.restore:
+            andVer = andVerCheck()
+            restoreApp(andVer, args.app)
+            cleanup = raw_input("[*]  Would you like to cleanup? WARNING: This will DELETE all folders associated with the application backup/restore process. (y/n)")
+            if cleanup == "y":
+                cleanup(args.app)
+            else:
+                print("[-] Leaving artifacts as they are. Manually cleanup if you need.")
+        else:
+            print "[-] Missing backup or restore argument."
     elif args.listapps == True:
         listApps()
     else:
